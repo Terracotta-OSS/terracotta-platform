519fa9bf96746389b7982287fb6c2632a13f6d4b
Mathieu Carbou <mathieu.carbou@gmail.com>
2016-04-14 13:28:55 -0400

    Moved ClientIdentifier in cluster package, like it was before

--- management-model/src/main/java/org/terracotta/management/model/cluster/ClientIdentifier.java
+++ management-model/src/main/java/org/terracotta/management/model/cluster/ClientIdentifier.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terracotta.management.model;
+package org.terracotta.management.model.cluster;
+
+import org.terracotta.management.model.Objects;
 
 import javax.xml.bind.DatatypeConverter;
 import java.io.ByteArrayOutputStream;
