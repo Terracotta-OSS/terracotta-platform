67a35064eb13bf399d8feac3901f22b294748fac
Ludovic Orban <lorban@bitronix.be>
2016-09-22 12:27:39 +0200

    use our own UTF encoding

--- runnel/src/main/java/org/terracotta/runnel/encoding/dataholders/StringDataHolder.java
+++ runnel/src/main/java/org/terracotta/runnel/encoding/dataholders/StringDataHolder.java
@@ -17,21 +17,20 @@ package org.terracotta.runnel.encoding.dataholders;
 
 import org.terracotta.runnel.utils.WriteBuffer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
 
 /**
  * @author Ludovic Orban
  */
 public class StringDataHolder extends AbstractDataHolder {
 
-  private static final Charset UTF8 = Charset.forName("UTF-8");
-
   private final ByteBuffer encodedString;
 
   public StringDataHolder(String value, int index) {
     super(index);
-    this.encodedString = UTF8.encode(value);
+    this.encodedString = encodeString(value);
   }
 
   @Override
@@ -43,4 +42,44 @@ public class StringDataHolder extends AbstractDataHolder {
   protected void encodeValue(WriteBuffer writeBuffer) {
     writeBuffer.putByteBuffer(encodedString);
   }
+
+
+  private ByteBuffer encodeString(String object) {
+    ByteArrayOutputStream bout = new ByteArrayOutputStream(object.length());
+    try {
+      int length = object.length();
+      int i = 0;
+
+      for (; i < length; i++) {
+        char c = object.charAt(i);
+        if ((c == 0x0000) || (c > 0x007f)) break;
+        bout.write(c);
+      }
+
+      for (; i < length; i++) {
+        char c = object.charAt(i);
+        if (c == 0x0000) {
+          bout.write(0xc0);
+          bout.write(0x80);
+        } else if (c < 0x0080) {
+          bout.write(c);
+        } else if (c < 0x800) {
+          bout.write(0xc0 | ((c >>> 6) & 0x1f));
+          bout.write(0x80 | (c & 0x3f));
+        } else {
+          bout.write(0xe0 | ((c >>> 12) & 0x1f));
+          bout.write(0x80 | ((c >>> 6) & 0x3f));
+          bout.write(0x80 | (c & 0x3f));
+        }
+      }
+    } finally {
+      try {
+        bout.close();
+      } catch (IOException ex) {
+        throw new AssertionError(ex);
+      }
+    }
+    return ByteBuffer.wrap(bout.toByteArray());
+  }
+
 }
