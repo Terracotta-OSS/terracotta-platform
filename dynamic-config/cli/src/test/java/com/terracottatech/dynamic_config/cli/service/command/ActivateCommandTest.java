b9bd7716d1aa607b34b701519856442ad9f04b01
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-07-23 10:20:58 -0400

    TDB-4607: Missing header

--- dynamic-config/cli/src/test/java/com/terracottatech/dynamic_config/cli/service/command/ActivateCommandTest.java
+++ dynamic-config/cli/src/test/java/com/terracottatech/dynamic_config/cli/service/command/ActivateCommandTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2011-2019 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
+ * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
+ */
 package com.terracottatech.dynamic_config.cli.service.command;
 
 import com.terracottatech.dynamic_config.cli.Injector;
