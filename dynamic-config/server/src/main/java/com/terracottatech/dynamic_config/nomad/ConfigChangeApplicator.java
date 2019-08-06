7a11dccca378b2b75443baf9a2b64b5fb6cf78c0
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-08-06 12:36:43 -0400

    TDB-4594: Bugfix: Missing rejection in case the config is null

--- dynamic-config/server/src/main/java/com/terracottatech/dynamic_config/nomad/ConfigChangeApplicator.java
+++ dynamic-config/server/src/main/java/com/terracottatech/dynamic_config/nomad/ConfigChangeApplicator.java
@@ -42,7 +42,7 @@ public class ConfigChangeApplicator implements ChangeApplicator<String> {
       }
     }
 
-    return allow(baseConfig);
+    return baseConfig == null ? reject("Change rejected: " + change) : allow(baseConfig);
   }
 
   @Override
