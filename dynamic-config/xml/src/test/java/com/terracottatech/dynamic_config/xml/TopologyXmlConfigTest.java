313500db11a5a54c4f7f7a0cee3d6d608c12a70b
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-08-07 04:09:12 -0400

    TDB-4594: Added test & test data

--- dynamic-config/xml/src/test/java/com/terracottatech/dynamic_config/xml/TopologyXmlConfigTest.java
+++ dynamic-config/xml/src/test/java/com/terracottatech/dynamic_config/xml/TopologyXmlConfigTest.java
@@ -27,28 +27,45 @@ import static org.xmlunit.matchers.CompareMatcher.isSimilarTo;
  */
 public class TopologyXmlConfigTest {
 
-  private Topology topology;
-  private String xml;
+  private Topology topology1, topology2;
+  private String xml1, xml2;
 
   @Before
   public void setUp() throws Exception {
-    topology = Json.parse(getClass().getResource("/topology.json"), Topology.class);
-    xml = new String(Files.readAllBytes(Paths.get(getClass().getResource("/multi-stripe-config.xml").toURI())), StandardCharsets.UTF_8);
+    topology1 = Json.parse(getClass().getResource("/topology1.json"), Topology.class);
+    xml1 = new String(Files.readAllBytes(Paths.get(getClass().getResource("/topology1.xml").toURI())), StandardCharsets.UTF_8);
+    topology2 = Json.parse(getClass().getResource("/topology2.json"), Topology.class);
+    xml2 = new String(Files.readAllBytes(Paths.get(getClass().getResource("/topology2.xml").toURI())), StandardCharsets.UTF_8);
   }
 
   @Test
-  public void toXml() {
-    String actual = TopologyXmlConfig.toXml(Paths.get(""), topology);
-    assertThat(actual, actual, isSimilarTo(Input.from(xml))
+  public void toXml1() {
+    String actual = TopologyXmlConfig.toXml(Paths.get(""), topology1);
+    assertThat(actual, actual, isSimilarTo(Input.from(xml1))
         .ignoreComments()
         .ignoreWhitespace()
         .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndText)));
   }
 
   @Test
-  public void fromXml() {
-    Topology actual = TopologyXmlConfig.fromXml("node-1", xml);
-    assertThat(toPrettyJson(actual), actual, is(equalTo(topology)));
+  public void fromXml1() {
+    Topology actual = TopologyXmlConfig.fromXml("node-1", xml1);
+    assertThat(toPrettyJson(actual), actual, is(equalTo(topology1)));
+  }
+
+  @Test
+  public void toXml2() {
+    String actual = TopologyXmlConfig.toXml(Paths.get(""), topology2);
+    assertThat(actual, actual, isSimilarTo(Input.from(xml2))
+        .ignoreComments()
+        .ignoreWhitespace()
+        .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndText)));
+  }
+
+  @Test
+  public void fromXml2() {
+    Topology actual = TopologyXmlConfig.fromXml("node-1", xml2);
+    assertThat(toPrettyJson(actual), actual, is(equalTo(topology2)));
   }
 
 }
