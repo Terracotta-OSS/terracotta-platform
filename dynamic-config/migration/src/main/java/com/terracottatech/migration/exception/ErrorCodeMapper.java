27cdd2212d59845f916ae6124c1f0b73bb86b45e
Tathagata Chakraborty <27780754+chaktatha@users.noreply.github.com>
2019-09-04 15:25:13 +0530

    Changing error enums based on review comments

--- dynamic-config/migration/src/main/java/com/terracottatech/migration/exception/ErrorCodeMapper.java
+++ dynamic-config/migration/src/main/java/com/terracottatech/migration/exception/ErrorCodeMapper.java
@@ -7,16 +7,16 @@ package com.terracottatech.migration.exception;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.terracottatech.migration.exception.ErrorCode.BOTH_WHITELIST_DEPR_WHITELIST_PRESENT;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIR;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIR_RESOURCE_NUMBERS;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_LEASE_TIME_CONFIGURATION;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFF_HEAPS;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFF_HEAP_RESOURCE_NUMBERS;
+import static com.terracottatech.migration.exception.ErrorCode.BOTH_WHITELIST_AND_DEPR_WHITELIST_PRESENT;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIRS;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIR_NUMBERS;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_CLIENT_LEASE_DURATION;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFFHEAP_RESOURCES;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFFHEAP_RESOURCE_NUMBERS;
 import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_SECURITY_CONFIGURATION;
 import static com.terracottatech.migration.exception.ErrorCode.MULTIPLE_PLATFORM_DATA_DIRS;
-import static com.terracottatech.migration.exception.ErrorCode.NON_UNIQUE_PLATFORM_DATA_ROOT_NAME;
-import static com.terracottatech.migration.exception.ErrorCode.PLATFORM_DATA_ROOT_MISSING_IN_SOME_OF_THE_CONFIG_FILES;
+import static com.terracottatech.migration.exception.ErrorCode.NON_UNIQUE_PLATFORM_DATA_DIR_NAME;
+import static com.terracottatech.migration.exception.ErrorCode.PLATFORM_DATA_DIR_MISSING_IN_SOME_CONFIG_FILES;
 import static com.terracottatech.migration.exception.ErrorCode.UNKNOWN_ERROR;
 
 public class ErrorCodeMapper {
@@ -25,31 +25,31 @@ public class ErrorCodeMapper {
 
   static {
     //Data Root Validation Error Code Mappings
-    ERROR_MAP.put(800001L, new ErrorDetail(MISMATCHED_DATA_DIR_RESOURCE_NUMBERS
+    ERROR_MAP.put(800001L, new ErrorDetail(MISMATCHED_DATA_DIR_NUMBERS
         , "Number of data directories defined in %s and in %s are not matching"));
-    ERROR_MAP.put(800002L, new ErrorDetail(PLATFORM_DATA_ROOT_MISSING_IN_SOME_OF_THE_CONFIG_FILES
+    ERROR_MAP.put(800002L, new ErrorDetail(PLATFORM_DATA_DIR_MISSING_IN_SOME_CONFIG_FILES
         , "Platform data root missing in one of %s %s"));
-    ERROR_MAP.put(800003L, new ErrorDetail(MISMATCHED_DATA_DIR
+    ERROR_MAP.put(800003L, new ErrorDetail(MISMATCHED_DATA_DIRS
         , "Data root directories are not same in %s and %s"));
-    ERROR_MAP.put(800004L, new ErrorDetail(NON_UNIQUE_PLATFORM_DATA_ROOT_NAME
+    ERROR_MAP.put(800004L, new ErrorDetail(NON_UNIQUE_PLATFORM_DATA_DIR_NAME
         , "Different data root directories are defined in %s %s"));
     ERROR_MAP.put(800005L, new ErrorDetail(MULTIPLE_PLATFORM_DATA_DIRS,
         "Multiple data root directories are defined in %s"));
 
     //Security Validation Error Code Mappings
-    ERROR_MAP.put(900001L, new ErrorDetail(BOTH_WHITELIST_DEPR_WHITELIST_PRESENT
+    ERROR_MAP.put(900001L, new ErrorDetail(BOTH_WHITELIST_AND_DEPR_WHITELIST_PRESENT
         , "Both elements <whitelist> and <white-list>"));
     ERROR_MAP.put(900002L, new ErrorDetail(MISMATCHED_SECURITY_CONFIGURATION
         , "Mismatched Security configuration in %s"));
 
     //Lease
-    ERROR_MAP.put(700001L, new ErrorDetail(MISMATCHED_LEASE_TIME_CONFIGURATION
+    ERROR_MAP.put(700001L, new ErrorDetail(MISMATCHED_CLIENT_LEASE_DURATION
         , "Mismatched lease configuration in %s"));
 
     //OffHeap
-    ERROR_MAP.put(500001L, new ErrorDetail(MISMATCHED_OFF_HEAP_RESOURCE_NUMBERS
+    ERROR_MAP.put(500001L, new ErrorDetail(MISMATCHED_OFFHEAP_RESOURCE_NUMBERS
         , "Different number of off-heap resources are defined in %s and %s"));
-    ERROR_MAP.put(500002L, new ErrorDetail(MISMATCHED_OFF_HEAPS
+    ERROR_MAP.put(500002L, new ErrorDetail(MISMATCHED_OFFHEAP_RESOURCES
         , "Mismatched off-heap configuration in %s and %s"));
 
   }
--- dynamic-config/migration/src/test/java/com/terracottatech/migration/exception/ErrorCodeMapperTest.java
+++ dynamic-config/migration/src/test/java/com/terracottatech/migration/exception/ErrorCodeMapperTest.java
@@ -6,16 +6,16 @@ package com.terracottatech.migration.exception;
 
 import org.junit.Test;
 
-import static com.terracottatech.migration.exception.ErrorCode.BOTH_WHITELIST_DEPR_WHITELIST_PRESENT;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIR;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIR_RESOURCE_NUMBERS;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_LEASE_TIME_CONFIGURATION;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFF_HEAPS;
-import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFF_HEAP_RESOURCE_NUMBERS;
+import static com.terracottatech.migration.exception.ErrorCode.BOTH_WHITELIST_AND_DEPR_WHITELIST_PRESENT;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIRS;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_DATA_DIR_NUMBERS;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_CLIENT_LEASE_DURATION;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFFHEAP_RESOURCES;
+import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_OFFHEAP_RESOURCE_NUMBERS;
 import static com.terracottatech.migration.exception.ErrorCode.MISMATCHED_SECURITY_CONFIGURATION;
 import static com.terracottatech.migration.exception.ErrorCode.MULTIPLE_PLATFORM_DATA_DIRS;
-import static com.terracottatech.migration.exception.ErrorCode.NON_UNIQUE_PLATFORM_DATA_ROOT_NAME;
-import static com.terracottatech.migration.exception.ErrorCode.PLATFORM_DATA_ROOT_MISSING_IN_SOME_OF_THE_CONFIG_FILES;
+import static com.terracottatech.migration.exception.ErrorCode.NON_UNIQUE_PLATFORM_DATA_DIR_NAME;
+import static com.terracottatech.migration.exception.ErrorCode.PLATFORM_DATA_DIR_MISSING_IN_SOME_CONFIG_FILES;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -24,28 +24,28 @@ public class ErrorCodeMapperTest {
   @Test
   public void testGetErrorCode() {
     ErrorCodeMapper.ErrorDetail errorDetail = ErrorCodeMapper.getErrorCode(800001L);
-    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_DATA_DIR_RESOURCE_NUMBERS));
+    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_DATA_DIR_NUMBERS));
 
     errorDetail = ErrorCodeMapper.getErrorCode(800002L);
-    assertThat(errorDetail.getErrorCode(), is(PLATFORM_DATA_ROOT_MISSING_IN_SOME_OF_THE_CONFIG_FILES));
+    assertThat(errorDetail.getErrorCode(), is(PLATFORM_DATA_DIR_MISSING_IN_SOME_CONFIG_FILES));
     errorDetail = ErrorCodeMapper.getErrorCode(800003L);
-    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_DATA_DIR));
+    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_DATA_DIRS));
     errorDetail = ErrorCodeMapper.getErrorCode(800004L);
-    assertThat(errorDetail.getErrorCode(), is(NON_UNIQUE_PLATFORM_DATA_ROOT_NAME));
+    assertThat(errorDetail.getErrorCode(), is(NON_UNIQUE_PLATFORM_DATA_DIR_NAME));
     errorDetail = ErrorCodeMapper.getErrorCode(800005L);
     assertThat(errorDetail.getErrorCode(), is(MULTIPLE_PLATFORM_DATA_DIRS));
 
     errorDetail = ErrorCodeMapper.getErrorCode(900001L);
-    assertThat(errorDetail.getErrorCode(), is(BOTH_WHITELIST_DEPR_WHITELIST_PRESENT));
+    assertThat(errorDetail.getErrorCode(), is(BOTH_WHITELIST_AND_DEPR_WHITELIST_PRESENT));
     errorDetail = ErrorCodeMapper.getErrorCode(900002L);
     assertThat(errorDetail.getErrorCode(), is(MISMATCHED_SECURITY_CONFIGURATION));
 
     errorDetail = ErrorCodeMapper.getErrorCode(700001L);
-    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_LEASE_TIME_CONFIGURATION));
+    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_CLIENT_LEASE_DURATION));
 
     errorDetail = ErrorCodeMapper.getErrorCode(500001L);
-    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_OFF_HEAP_RESOURCE_NUMBERS));
+    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_OFFHEAP_RESOURCE_NUMBERS));
     errorDetail = ErrorCodeMapper.getErrorCode(500002L);
-    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_OFF_HEAPS));
+    assertThat(errorDetail.getErrorCode(), is(MISMATCHED_OFFHEAP_RESOURCES));
   }
 }
