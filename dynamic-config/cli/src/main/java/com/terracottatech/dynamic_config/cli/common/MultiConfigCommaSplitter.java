27a5115dcfa68d1672cda52c0aa0c922073813ef
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-11-14 10:11:11 -0500

    TDB-4630: Updated MultiConfigCommaSplitter

--- dynamic-config/cli/src/main/java/com/terracottatech/dynamic_config/cli/common/MultiConfigCommaSplitter.java
+++ dynamic-config/cli/src/main/java/com/terracottatech/dynamic_config/cli/common/MultiConfigCommaSplitter.java
@@ -6,37 +6,64 @@ package com.terracottatech.dynamic_config.cli.common;
 
 import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.converters.IParameterSplitter;
+import com.terracottatech.dynamic_config.model.Setting;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
- * A splitter which splits the input about a comma, and transforms it into a multi-config representation. For example,
- * data-dirs=foo:bar,foo2:bar2 becomes {data-dirs.foo=bar, data-dirs.foo2=bar2}
+ * A splitter which splits the input about a comma, and transforms it into a multi-config representation.
+ * <p>
+ * The splitter is only applied in case of a map setting " ({@link Setting#isMap() == true}
+ * <p>
+ * For example, data-dirs=foo:bar,foo2:bar2 becomes {data-dirs.foo=bar, data-dirs.foo2=bar2}
  */
 public class MultiConfigCommaSplitter implements IParameterSplitter {
   public List<String> split(String value) {
-    List<String> properties = new ArrayList<>();
+    String[] keyValue = value.split("=", 2);
+
+    if (keyValue.length != 2) {
+      // let the next code parse the value and do the correct checks
+      return Collections.singletonList(value);
+    }
+
+    if (!isMap(keyValue[0])) {
+      // let the next code parse the value and do the correct checks if this setting is not a map
+      return Collections.singletonList(value);
+    }
+
+    // here we only have inputs like:
+    // - data-dirs=foo:bar
+    // - data-dirs=foo:bar,foo2:bar2
+    // or invalid things like:
+    // - data-dirs=foo:
+    // - data-dirs=:
+    // - data-dirs=d
+    // - etc
+    return Stream.of(keyValue[1].split(","))
+        .filter(s -> !s.trim().isEmpty()) // in case user sends a lot of commas: data-dirs=,foo:bar,,foo2:bar2,,
+        .map(s -> {
+          String[] nameProperty = s.trim().split(":", 2);
+          if (nameProperty.length != 2) {
+            // we always expect a key and value for maps
+            throw new ParameterException("Invalid input: " + value);
+          }
+          return keyValue[0] + "." + nameProperty[0] + "=" + nameProperty[1];
+        })
+        .distinct() // in case user sends: data-dirs=foo:bar,foo:bar
+        .collect(toList());
+  }
+
+  private boolean isMap(String settingName) {
     try {
-      String[] keyValue = value.split("=");
-      if (keyValue.length == 1) {
-        // No '=' specified, which would be case in get and unset commands
-        return Collections.singletonList(value);
-      }
-
-      String[] values = keyValue[1].split(",");
-      if (values.length == 1 && !values[0].contains(":")) {
-        return Collections.singletonList(value);
-      }
-
-      for (String item : values) {
-        String[] nameProperty = item.split(":");
-        properties.add(keyValue[0] + "." + nameProperty[0] + "=" + nameProperty[1]);
-      }
-      return properties;
-    } catch (Exception e) {
-      throw new ParameterException("Invalid input: " + value, e);
+      // try to get the setting name and see if this is a map
+      return Setting.fromName(settingName).isMap();
+    } catch (RuntimeException e) {
+      // if we fail parsing the setting name, consider this is not a map
+      return false;
     }
   }
 }
--- dynamic-config/cli/src/test/java/com/terracottatech/dynamic_config/cli/common/MultiConfigCommaSplitterTest.java
+++ dynamic-config/cli/src/test/java/com/terracottatech/dynamic_config/cli/common/MultiConfigCommaSplitterTest.java
@@ -19,43 +19,49 @@ public class MultiConfigCommaSplitterTest {
   @Test
   public void testSplit_1() {
     List<String> split = splitter.split("offheap-resources.main");
-    assertEquals(split, singletonList("offheap-resources.main"));
+    assertEquals(singletonList("offheap-resources.main"), split);
   }
 
   @Test
   public void testSplit_2() {
     List<String> split = splitter.split("offheap-resources");
-    assertEquals(split, singletonList("offheap-resources"));
+    assertEquals(singletonList("offheap-resources"), split);
   }
 
   //<--Set command usages-->
   @Test
   public void testSplit_3() {
     List<String> split = splitter.split("offheap-resources.main=512MB");
-    assertEquals(split, singletonList("offheap-resources.main=512MB"));
+    assertEquals(singletonList("offheap-resources.main=512MB"), split);
   }
 
   @Test
   public void testSplit_4() {
     List<String> split = splitter.split("offheap-resources=main:512MB");
-    assertEquals(split, singletonList("offheap-resources.main=512MB"));
+    assertEquals(singletonList("offheap-resources.main=512MB"), split);
   }
 
   @Test
   public void testSplit_5() {
     List<String> split = splitter.split("offheap-resources=main:512MB,second:1GB");
-    assertEquals(split, asList("offheap-resources.main=512MB", "offheap-resources.second=1GB"));
+    assertEquals(asList("offheap-resources.main=512MB", "offheap-resources.second=1GB"), split);
   }
 
   @Test
   public void testSplit_6() {
     List<String> split = splitter.split("stripe.1.node.1.tc-properties.something=value");
-    assertEquals(split, singletonList("stripe.1.node.1.tc-properties.something=value"));
+    assertEquals(singletonList("stripe.1.node.1.tc-properties.something=value"), split);
   }
 
   @Test
   public void testSplit_7() {
     List<String> split = splitter.split("client-reconnect-window=10s");
-    assertEquals(split, singletonList("client-reconnect-window=10s"));
+    assertEquals(singletonList("client-reconnect-window=10s"), split);
+  }
+
+  @Test
+  public void testSplit_8() {
+    List<String> split = splitter.split("failover-priority=consistency:2");
+    assertEquals(singletonList("failover-priority=consistency:2"), split);
   }
 }
