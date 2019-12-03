c2edc544a7bc4533e3606f1976c46bc8a6673599
Mathieu Carbou <mathieu.carbou@gmail.com>
2019-12-03 13:07:37 -0500

    TDB-4703: Fixing path comparison for Windows

--- dynamic-config/common/src/test/java/com/terracottatech/dynamic_config/nomad/NomadChangeJsonTest.java
+++ dynamic-config/common/src/test/java/com/terracottatech/dynamic_config/nomad/NomadChangeJsonTest.java
@@ -16,7 +16,11 @@ import com.terracottatech.utilities.TimeUnit;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static com.terracottatech.dynamic_config.model.Setting.NODE_BACKUP_DIR;
 import static com.terracottatech.dynamic_config.model.Setting.OFFHEAP_RESOURCES;
@@ -33,7 +37,7 @@ public class NomadChangeJsonTest {
       .setClientReconnectWindow(60, TimeUnit.SECONDS).setOffheapResource("foo", 1, MemoryUnit.GB)));
 
   @Test
-  public void test_ser_deser() throws IOException {
+  public void test_ser_deser() throws IOException, URISyntaxException {
     NomadChange[] changes = {
         new ClusterActivationNomadChange(cluster),
         new ConfigMigrationNomadChange(cluster),
@@ -49,10 +53,21 @@ public class NomadChangeJsonTest {
 
     for (int i = 0; i < changes.length; i++) {
       NomadChange change = changes[i];
+
       URL jsonFile = getClass().getResource("/nomad/change" + i + ".json");
-      assertThat(jsonFile.getPath(), objectMapper.valueToTree(change).toString(), is(equalTo(objectMapper.readTree(jsonFile).toString())));
-      assertThat(jsonFile.getPath(), objectMapper.readValue(jsonFile, NomadChange.class), is(equalTo(change)));
+      byte[] bytes = Files.readAllBytes(Paths.get(jsonFile.toURI()));
+      String json = new String(bytes, StandardCharsets.UTF_8);
+
+      if (isWindows()) {
+        json = json.replace("/", "\\\\");
+      }
+
+      assertThat(jsonFile.getPath(), objectMapper.valueToTree(change).toString(), is(equalTo(objectMapper.readTree(json).toString())));
+      assertThat(jsonFile.getPath(), objectMapper.readValue(json, NomadChange.class), is(equalTo(change)));
     }
   }
 
+  private static boolean isWindows() {
+    return System.getProperty("os.name").toLowerCase().startsWith("windows");
+  }
 }
