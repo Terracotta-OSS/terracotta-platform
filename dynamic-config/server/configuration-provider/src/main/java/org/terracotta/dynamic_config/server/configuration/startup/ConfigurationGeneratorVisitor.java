ff0ddb05af90981d3e4a14f60d50996b07f7af29
Venkata Sairam, Madduri <VenkataSairam.Madduri@softwareag.com>
2020-06-03 20:06:38 +0530

    Ensure DynamicConfigConfigurationProvider returns latest committed configuration

--- dynamic-config/server/configuration-provider/src/main/java/org/terracotta/dynamic_config/server/configuration/startup/ConfigurationGeneratorVisitor.java
+++ dynamic-config/server/configuration-provider/src/main/java/org/terracotta/dynamic_config/server/configuration/startup/ConfigurationGeneratorVisitor.java
@@ -47,6 +47,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static java.lang.System.lineSeparator;
@@ -96,10 +97,10 @@ public class ConfigurationGeneratorVisitor {
 
     if (unConfiguredMode) {
       // in diagnostic / unconfigured node mode, make sure we make platform think that the node is alone...
-      return new StartupConfiguration(nodeContext.alone(), unConfiguredMode, repairMode, classLoader, pathResolver, parameterSubstitutor, objectMapperFactory);
+      return new StartupConfiguration(() -> nodeContext.alone(), unConfiguredMode, repairMode, classLoader, pathResolver, parameterSubstitutor, objectMapperFactory);
     }
 
-    NodeContext nodeContext = nomadServerManager.getConfiguration()
+    Supplier<NodeContext> nodeContextSupplier = () -> nomadServerManager.getConfiguration()
         .orElseThrow(() -> new IllegalStateException("Node has not been activated or migrated properly: unable find the latest committed configuration to use at startup. Please delete the configuration directory and try again."));
 
     if (repairMode) {
@@ -107,10 +108,10 @@ public class ConfigurationGeneratorVisitor {
       // - the node won't be activated (Nomad 2 phase commit system won't be available)
       // - the diagnostic port will be available for the repair command to be able to rewrite the append log
       // - the config created will be stripped to make platform think this node is alone;
-      nodeContext = nodeContext.alone();
+      nodeContextSupplier = () -> nodeContext.alone();
     }
 
-    return new StartupConfiguration(nodeContext, unConfiguredMode, repairMode, classLoader, pathResolver, parameterSubstitutor, objectMapperFactory);
+    return new StartupConfiguration(nodeContextSupplier, unConfiguredMode, repairMode, classLoader, pathResolver, parameterSubstitutor, objectMapperFactory);
   }
 
   void startUnconfigured(NodeContext nodeContext, String optionalNodeConfigurationDirFromCLI) {
