06e66676f77f9606466cd1a466bdebe101bc6175
Myron Scott <myron.scott@softwareag.com>
2020-06-04 10:25:15 -0700

    log to console if unconfigured

--- dynamic-config/server/configuration-provider/src/main/java/org/terracotta/dynamic_config/server/configuration/startup/StartupConfiguration.java
+++ dynamic-config/server/configuration-provider/src/main/java/org/terracotta/dynamic_config/server/configuration/startup/StartupConfiguration.java
@@ -312,7 +312,7 @@ public class StartupConfiguration implements Configuration, PrettyPrintable, Sta
 
       @Override
       public File getLogsLocation() {
-        return substitutor.substitute(pathResolver.resolve(node.getNodeLogDir())).toFile();
+        return (unConfigured) ? null : substitutor.substitute(pathResolver.resolve(node.getNodeLogDir())).toFile();
       }
 
       @Override
