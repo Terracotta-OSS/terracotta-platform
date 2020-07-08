10cb8382760e8261ea8681f875258c1f0d48200a
Mathieu Carbou <mathieu.carbou@gmail.com>
2020-07-08 19:51:23 -0400

    Moving Nomad processors to the right module

--- dynamic-config/server/configuration-provider/src/test/java/org/terracotta/dynamic_config/server/configuration/service/nomad/processor/ClusterActivationNomadChangeProcessorTest.java
+++ dynamic-config/server/configuration-provider/src/test/java/org/terracotta/dynamic_config/server/configuration/service/nomad/processor/ClusterActivationNomadChangeProcessorTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terracotta.dynamic_config.server.configuration.nomad.processor;
+package org.terracotta.dynamic_config.server.configuration.service.nomad.processor;
 
 import org.junit.Before;
 import org.junit.Rule;
