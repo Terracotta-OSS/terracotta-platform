45d0db0a265d8b691b87af0bb5712e37d49522e9
Saurabh Agarwal <Saurabh.Agarwal@softwareag.com>
2020-07-28 12:26:16 +0530

    Display even-sum warning regardless of cluster activation status and current number of nodes in attach/detach command

--- dynamic-config/cli/config-tool/src/main/java/org/terracotta/dynamic_config/cli/config_tool/command/DetachCommand.java
+++ dynamic-config/cli/config-tool/src/main/java/org/terracotta/dynamic_config/cli/config_tool/command/DetachCommand.java
@@ -76,18 +76,18 @@ public class DetachCommand extends TopologyCommand {
       }
 
       FailoverPriority failoverPriority = destinationCluster.getFailoverPriority();
-      if (failoverPriority.equals(consistency()) && destinationClusterActivated) {
+      if (failoverPriority.equals(consistency())) {
         int voterCount = failoverPriority.getVoters();
         int nodeCount = destinationStripe.getNodes().size();
         int sum = voterCount + nodeCount;
         if (sum % 2 != 0) {
           logger.warn(lineSeparator() +
-              "==============================================================================" + lineSeparator() +
-              "WARNING: The sum (" + sum + ") of voter count (" + voterCount + ") and number of nodes " +
+              "===================================================================================" + lineSeparator() +
+              "IMPORTANT: The sum (" + sum + ") of voter count (" + voterCount + ") and number of nodes " +
               "(" + nodeCount + ") in this stripe " + lineSeparator() +
-              "is an odd number, but will become even with the removal of node " + source + lineSeparator() + ". " +
-              "An even-numbered configuration is more likely to experience split-brain situations." +
-              "==============================================================================" + lineSeparator());
+              "is an odd number, which will become even with the removal of node " + source + "." + lineSeparator() +
+              "An even-numbered configuration is more likely to experience split-brain situations." + lineSeparator() +
+              "===================================================================================" + lineSeparator());
         }
       }
 
