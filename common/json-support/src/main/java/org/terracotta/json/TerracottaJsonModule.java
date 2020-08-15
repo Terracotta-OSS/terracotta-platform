4e923f23653b85d3b53feecb4e1093f3a8739406
Mathieu Carbou <mathieu.carbou@gmail.com>
2020-08-15 00:27:44 -0400

    TDB-5184: Fix JSON Path serializer/deserializer + support backward compatibility The old one was not able to correctly deserialize on Linux a Path serialized on Windows

--- common/json-support/src/main/java/org/terracotta/json/TerracottaJsonModule.java
+++ common/json-support/src/main/java/org/terracotta/json/TerracottaJsonModule.java
@@ -15,14 +15,23 @@
  */
 package org.terracotta.json;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * @author Mathieu Carbou
@@ -32,13 +41,49 @@ public class TerracottaJsonModule extends SimpleModule {
 
   public TerracottaJsonModule() {
     super(TerracottaJsonModule.class.getSimpleName(), new Version(1, 0, 0, null, null, null));
-    addSerializer(Path.class, ToStringSerializer.instance);
-    addDeserializer(Path.class, new FromStringDeserializer<Path>(Path.class) {
+    addSerializer(Path.class, new StdSerializer<Path>(Path.class) {
       private static final long serialVersionUID = 1L;
 
       @Override
-      protected Path _deserialize(String value, DeserializationContext ctxt) {
-        return Paths.get(value);
+      public void serialize(Path value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeStartArray(value.getNameCount());
+        for (Path segment : value) {
+          gen.writeString(segment.toString());
+        }
+        gen.writeEndArray();
+      }
+    });
+    addDeserializer(Path.class, new StdDeserializer<Path>(Path.class) {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public Path deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        Deque<String> segments = new ArrayDeque<>();
+        try {
+          if (p.getCurrentToken() == JsonToken.VALUE_STRING) {
+            // backward compat with V1
+            segments.add(p.getText());
+          } else {
+            // new parsing, using arrays
+            while (true) {
+              // optimization for collections of strings
+              String value = p.nextTextValue();
+              if (value != null) {
+                segments.add(value);
+              } else if (p.getCurrentToken() == JsonToken.END_ARRAY) {
+                break;
+              } else {
+                value = _parseString(p, ctxt);
+                segments.add(value);
+              }
+            }
+          }
+        } catch (Exception e) {
+          throw JsonMappingException.wrapWithPath(e, segments, segments.size());
+        }
+        String first = segments.removeFirst();
+        String[] others = segments.toArray(new String[0]);
+        return Paths.get(first, others);
       }
     });
   }
