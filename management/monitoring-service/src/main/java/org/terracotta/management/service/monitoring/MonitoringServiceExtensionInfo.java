41f0b4acf137e8cf61341c20001c9be3a24a99df
Tom Mesic <59479956+tmesic99@users.noreply.github.com>
2021-05-13 15:49:16 -0400

    support jar version/info reporting

---
+++ management/monitoring-service/src/main/java/org/terracotta/management/service/monitoring/MonitoringServiceExtensionInfo.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Terracotta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terracotta.management.service.monitoring;
+
+import com.tc.productinfo.ExtensionInfo;
+import org.terracotta.dynamic_config.server.api.ManifestInfo;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.System.lineSeparator;
+
+public class MonitoringServiceExtensionInfo implements ExtensionInfo {
+
+  private static final String PLUGIN_NAME = "Monitoring Service";
+
+  @Override
+  public String getExtensionInfo() {
+    Map<String, String> attributes = ManifestInfo.getJarManifestInfo(this.getClass());
+    return PLUGIN_NAME + ":" + lineSeparator() + Stream.of(ManifestInfo.BASE_ATTRIBUTES)
+        .filter(attributes::containsKey)
+        .map(n -> n + ": " + attributes.get(n))
+        .collect(Collectors.joining(lineSeparator())) + lineSeparator();
+  }
+
+  @Override
+  public String getValue(String name) {
+    if (name.equals(DESCRIPTION)) {
+      return getExtensionInfo();
+    } else {
+      return "";
+    }
+  }
+}
