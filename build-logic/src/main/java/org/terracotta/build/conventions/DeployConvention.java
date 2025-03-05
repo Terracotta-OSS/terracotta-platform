599db45351cb0503c38608c1ea6a2e0c0a89ab0a
Mathieu Carbou <mathieu.carbou@ibm.com>
2025-03-05 23:31:27 +0100

    Add support for GPG signing of deployable artifacts with signMavenJavaPublication task

--- build-logic/src/main/java/org/terracotta/build/conventions/DeployConvention.java
+++ build-logic/src/main/java/org/terracotta/build/conventions/DeployConvention.java
@@ -1,6 +1,11 @@
 package org.terracotta.build.conventions;
 
+import java.util.Optional;
+
 import org.gradle.api.Project;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.plugins.signing.SigningExtension;
+import org.gradle.plugins.signing.SigningPlugin;
 import org.terracotta.build.plugins.DeployPlugin;
 
 /**
@@ -17,5 +22,24 @@ public class DeployConvention implements ConventionPlugin<Project, DeployPlugin>
   public void apply(Project project) {
     project.getPlugins().apply(BaseConvention.class);
     project.getPlugins().apply(DeployPlugin.class);
+    project.getPlugins().apply(SigningPlugin.class);
+
+    final String gpgSigningKey = Optional.ofNullable(System.getenv("GPG_SIGNING_KEY"))
+        .orElse(project.hasProperty("gpgSigningKey") ? project.property("gpgSigningKey").toString() : null);
+    final String gpgSigningPassphrase = Optional.ofNullable(System.getenv("GPG_SIGNING_PASSPHRASE")).orElse(
+        project.hasProperty("gpgSigningPassphrase") ? project.property("gpgSigningPassphrase").toString() : null);
+
+    if (gpgSigningKey != null && gpgSigningPassphrase != null) {
+      project.afterEvaluate(p -> {
+        p.getExtensions().configure(PublishingExtension.class, publishing -> {
+          if (!publishing.getPublications().isEmpty()) {
+            project.getExtensions().configure(SigningExtension.class, signing -> {
+              signing.useInMemoryPgpKeys(gpgSigningKey, gpgSigningPassphrase);
+              signing.sign(publishing.getPublications());
+            });
+          }
+        });
+      });
+    }
   }
 }
