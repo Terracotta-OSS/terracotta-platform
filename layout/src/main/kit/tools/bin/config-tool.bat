5fcc17aa62523d0315edda99d3330cb34b6e1f9a
Mathieu Carbou <mathieu.carbou@ibm.com>
2025-08-22 13:05:31 +0200

    License update: Parent's POM license definition requires all files to have the line Copyright IBM Corp. 2024, 2025

--- layout/src/main/kit/tools/bin/config-tool.bat
+++ layout/src/main/kit/tools/bin/config-tool.bat
@@ -1,6 +1,6 @@
 @REM
 @REM Copyright Terracotta, Inc.
-@REM Copyright Super iPaaS Integration LLC, an IBM Company 2024
+@REM Copyright IBM Corp. 2024, 2025
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
