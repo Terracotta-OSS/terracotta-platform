9fac272d1bdf861569cd72eef5596fe5ede396d9
Akshit Gupta <akshit.gupta@ibm.com>
2026-02-19 17:40:28 +0530

    TDB-19900: Add integration test for relay-mode SET command with auto-restart on activated nodes transitioning to PASSIVE-RELAY

--- dynamic-config/testing/integration-tests/src/test/java/org/terracotta/dynamic_config/system_tests/activated/SetCommand2x3IT.java
+++ dynamic-config/testing/integration-tests/src/test/java/org/terracotta/dynamic_config/system_tests/activated/SetCommand2x3IT.java
@@ -1,6 +1,6 @@
 /*
  * Copyright Terracotta, Inc.
- * Copyright IBM Corp. 2024, 2025
+ * Copyright IBM Corp. 2024, 2026
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@ import org.terracotta.dynamic_config.api.service.TopologyService;
 import org.terracotta.dynamic_config.test_support.ClusterDefinition;
 import org.terracotta.dynamic_config.test_support.DynamicConfigIT;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.function.Predicate.isEqual;
@@ -59,5 +61,35 @@ public class SetCommand2x3IT extends DynamicConfigIT {
         assertFalse(usingTopologyService(s, n, TopologyService::mustBeRestarted));
       }
     }
+  }
+
+  @Test
+  public void testSetMultipleRelayModeWithAutoRestart() {
+    waitForActive(1);
+    waitForActive(2);
+    waitForPassives(1);
+    waitForPassives(2);
+
+    int basePort = 9410;
+
+    List<String> args = new ArrayList<>(List.of("set", "-auto-restart", "-connect-to", "localhost:" + getNodePort(1, 1)));
+
+    for (int stripeId = 1; stripeId <= 2; stripeId++) {
+      for (int nodeId = 2; nodeId <= 3; nodeId++) {
+        String prefix = "stripe." + stripeId + ".node." + nodeId;
+        args.addAll(List.of("-setting", prefix + ".relay-mode=true"));
+        args.addAll(List.of("-setting", prefix + ".replica-hostname=localhost"));
+        args.addAll(List.of("-setting", prefix + ".replica-port=" + (basePort++)));
+      }
+    }
+
+    String[] cli = args.toArray(String[]::new);
+
+    assertThat(configTool(cli), is(successful()));
+
+    waitForPassiveRelay(1, 2);
+    waitForPassiveRelay(1, 3);
+    waitForPassiveRelay(2, 2);
+    waitForPassiveRelay(2, 3);
   }
 }
