5d584b76081b976f55bab563358adac49451a542
Mathieu Carbou <mathieu.carbou@ibm.com>
2026-03-25 17:01:32 +0100

    TDB-19412: Wire core security operations into the security logging

--- security/logger/server/services/src/main/java/org/terracotta/security/logger/SecurityLoggerServiceProvider.java
+++ security/logger/server/services/src/main/java/org/terracotta/security/logger/SecurityLoggerServiceProvider.java
@@ -17,6 +17,7 @@
 package org.terracotta.security.logger;
 
 import com.tc.classloader.BuiltinService;
+import com.tc.spi.Guardian;
 import org.terracotta.entity.PlatformConfiguration;
 import org.terracotta.entity.ServiceConfiguration;
 import org.terracotta.entity.ServiceProvider;
@@ -24,6 +25,8 @@ import org.terracotta.entity.ServiceProviderConfiguration;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
 
 @BuiltinService
 public class SecurityLoggerServiceProvider implements ServiceProvider {
@@ -37,23 +40,45 @@ public class SecurityLoggerServiceProvider implements ServiceProvider {
 
   @Override
   public <T> T getService(long consumerID, ServiceConfiguration<T> configuration) {
+    // provides SecurityLogger interface to other plugins
     if (configuration.getServiceType() == SecurityLogger.class) {
-      Collection<SecurityLogger> loggers = platformConfiguration.getExtendedConfiguration(SecurityLogger.class);
-      if (loggers.size() != 1) {
-        throw new IllegalStateException("Multiple or no instance found of " + SecurityLogger.class);
-      }
-      return configuration.getServiceType().cast(loggers.iterator().next());
+      return configuration.getServiceType().cast(findSecurityLogger().orElse(SecurityLogger.NOOP));
     }
+
+    // provide Guardian implementation to core
+    if (consumerID == 0 && configuration.getServiceType() == Guardian.class) {
+      return configuration.getServiceType().cast(new Guardian() {
+        @Override
+        public boolean validate(Op op, Properties properties) {
+          if (op == Op.SECURITY_OP) {
+            Object message = properties.remove("id");
+            if (message != null) {
+              findSecurityLogger().ifPresent(securityLogger -> securityLogger.log(message + ": {}", properties));
+            }
+          }
+          return true;
+        }
+      });
+    }
+
     throw new UnsupportedOperationException(configuration.getServiceType().getName());
   }
 
   @Override
   public Collection<Class<?>> getProvidedServiceTypes() {
-    return List.of(SecurityLogger.class);
+    return List.of(SecurityLogger.class, Guardian.class);
   }
 
   @Override
   public void prepareForSynchronization() {
     // no-op
+  }
+
+  private Optional<SecurityLogger> findSecurityLogger() {
+    Collection<SecurityLogger> loggers = platformConfiguration.getExtendedConfiguration(SecurityLogger.class);
+    if (loggers.size() != 1) {
+      throw new IllegalStateException("Multiple instances found for " + SecurityLogger.class);
+    }
+    return loggers.stream().findAny();
   }
 }
