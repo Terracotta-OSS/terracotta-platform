80d68c075115e854e893f04b4c98936f992864f9
Akshit Gupta <akshit.gupta@ibm.com>
2026-06-16 14:40:45 +0530

    Allow unsetting REPLICA_HOSTNAME and REPLICA_PORT for configured and activated clusters as user may want to remove the RELAY properties from the config

--- dynamic-config/model/src/test/java/org/terracotta/dynamic_config/api/model/ConfigurationTest.java
+++ dynamic-config/model/src/test/java/org/terracotta/dynamic_config/api/model/ConfigurationTest.java
@@ -526,8 +526,6 @@ public class ConfigurationTest {
       // set allowed for scope node
       Stream.of(
         tuple2(RELAY, "true"),
-        tuple2(REPLICA_HOSTNAME, "foo"),
-        tuple2(REPLICA_PORT, "9410"),
         tuple2(REPLICA, "true"),
         tuple2(RELAY_HOSTNAME, "foo"),
         tuple2(RELAY_PORT, "9410"),
@@ -543,6 +541,24 @@ public class ConfigurationTest {
 
         allowInput("stripe.1.node.1" + ns + tuple.t1, tuple.t1, NODE, 1, 1, null, null);
         rejectInput("stripe.1" + ns + tuple.t1 + "=", "Invalid input: 'stripe.1" + ns + tuple.t1 + "='. Reason: Setting '" + tuple.t1 + "' requires a value");
+        allowInput("stripe.1.node.1" + ns + tuple.t1 + "=" + tuple.t2, tuple.t1, NODE, 1, 1, null, tuple.t2);
+      });
+
+      // REPLICA_HOSTNAME, REPLICA_PORT are node-scoped and support UNSET (allow empty value)
+      Stream.of(
+        tuple2(REPLICA_HOSTNAME, "foo"),
+        tuple2(REPLICA_PORT, "9410")
+      ).forEach(tuple -> {
+        allowInput(tuple.t1.toString(), tuple.t1, CLUSTER, null, null, null, null);
+        allowInput(tuple.t1 + "=", tuple.t1, CLUSTER, null, null, null, null); // empty value allowed for UNSET
+        rejectInput(tuple.t1 + "=" + tuple.t2, "Invalid input: '" + tuple.t1 + "=" + tuple.t2 + "'. Reason: Setting '" + tuple.t1 + "' cannot be set at cluster level");
+
+        allowInput("stripe.1" + ns + tuple.t1, tuple.t1, STRIPE, 1, null, null, null);
+        allowInput("stripe.1" + ns + tuple.t1 + "=", tuple.t1, STRIPE, null, null, null, null); // empty value allowed for UNSET
+        rejectInput("stripe.1" + ns + tuple.t1 + "=" + tuple.t2, "Invalid input: 'stripe.1" + ns + tuple.t1 + "=" + tuple.t2 + "'. Reason: Setting '" + tuple.t1 + "' cannot be set at stripe level");
+
+        allowInput("stripe.1.node.1" + ns + tuple.t1, tuple.t1, NODE, 1, 1, null, null);
+        allowInput("stripe.1.node.1" + ns + tuple.t1 + "=", tuple.t1, NODE, 1, 1, null, null); // empty value allowed for UNSET
         allowInput("stripe.1.node.1" + ns + tuple.t1 + "=" + tuple.t2, tuple.t1, NODE, 1, 1, null, tuple.t2);
       });
     });
@@ -847,11 +863,15 @@ public class ConfigurationTest {
       }));
     });
 
-    // replica-hostname, replica-port: GET at any levels, SET at NODE level (CONFIGURING, ACTIVATED), IMPORT at NODE level (CONFIGURING)
+    // replica-hostname, replica-port: GET at any levels, SET/UNSET at NODE level (CONFIGURING, ACTIVATED), IMPORT at NODE level (CONFIGURING)
     Stream.of("replica-hostname", "replica-port").forEach(setting -> {
       Stream.of(CONFIGURING, ACTIVATED).forEach(state -> state.filter(GET).forEach(op -> NS.forEach(ns -> allow(state, op, ns + setting))));
 
-      Stream.of(CONFIGURING, ACTIVATED).forEach(state -> state.filter(UNSET).forEach(op -> NS.forEach(ns -> reject(state, op, ns + setting))));
+      Stream.of(CONFIGURING, ACTIVATED).forEach(state -> state.filter(UNSET).forEach(op -> {
+        reject(state, op, setting);
+        reject(state, op, "stripe.1." + setting);
+        allow(state, op, "stripe.1.node.1." + setting);
+      }));
 
       Stream.of(CONFIGURING, ACTIVATED).forEach(state -> state.filter(SET).forEach(op -> {
         reject(state, op, setting + "=1234");
@@ -860,7 +880,7 @@ public class ConfigurationTest {
         if (setting.contains("port")) {
           reject(state, op, "stripe.1.node.1." + setting + "=123456");
         }
-        reject(state, op, "stripe.1.node.1." + setting + "=");
+        reject(state, op, "stripe.1.node.1." + setting + "="); // SET requires a value
       }));
 
       Stream.of(CONFIGURING).forEach(state -> state.filter(IMPORT).forEach(op -> {
@@ -870,7 +890,7 @@ public class ConfigurationTest {
         if (setting.contains("port")) {
           reject(state, op, "stripe.1.node.1." + setting + "=123456");
         }
-        reject(state, op, "stripe.1.node.1." + setting + "=");
+        allow(state, op, "stripe.1.node.1." + setting + "=");
       }));
     });
 
