f54a8b436fe004d173c9282eb30d94f886cda0df
Akshit Gupta <akshit.gupta@ibm.com>
2026-06-16 18:37:50 +0530

    Add import command diagnostic integration tests for relay and replica

--- dynamic-config/testing/integration-tests/src/test/java/org/terracotta/dynamic_config/system_tests/diagnostic/ImportCommand1x2IT.java
+++ dynamic-config/testing/integration-tests/src/test/java/org/terracotta/dynamic_config/system_tests/diagnostic/ImportCommand1x2IT.java
@@ -55,4 +55,29 @@ public class ImportCommand1x2IT extends DynamicConfigIT {
       allOf(is(not(successful())),
         containsOutput("The relay setting is disabled for node with name: node-1-1, properties: {replica-hostname=null, replica-port=1234} are partially configured")));
   }
+
+  @Test
+  public void test_relay_invalid_mutual_exclusion() throws Exception {
+    Path configFile = copyConfigProperty("/config-property-files/1x1-relay-replica-invalid.properties");
+    assertThat(configTool("import", "-f", configFile.toString()),
+      allOf(is(not(successful())),
+        containsOutput("Node with name: node-1-1 has both relay and replica settings enabled")));
+  }
+
+  @Test
+  public void test_failed_import_with_multiple_nodes_per_stripe_replica_cluster() throws Exception {
+    // test cluster validation flow
+    Path configFile = copyConfigProperty("/config-property-files/1x2-replica-invalid1.properties");
+    assertThat(configTool("import", "-f", configFile.toString()),
+      allOf(not(successful()), containsOutput("Stripe with name: stripe1 has 2 nodes with names: node-1-1, node-1-2. A replica cluster can have at most 1 replica node per stripe")));
+  }
+
+  @Test
+  public void test_relay_import_on_replica_node() throws Exception {
+    stopNode(1, 1);
+    startNode(1, 1, getNewOptions(getNode(1, 1), "-replica", "true", "-relay-hostname", "localhost", "-relay-port", "1234", "-relay-group-port", "5678"));
+    Path configFile = copyConfigProperty("/config-property-files/1x1-relay.properties");
+    assertThat(configTool("import", "-f", configFile.toString()), is(successful()));
+    assertThat(configTool("export", "-s", getNode(1, 1).getHostPort()), allOf(is(successful()), containsOutput("relay=true")));
+  }
 }
